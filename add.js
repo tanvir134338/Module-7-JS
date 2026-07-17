235e322bf9a12bc5e30085cb3fe28e7d719afd6a
tanvir134338 <tanvir134338@gmail.com>
2026-07-18 03:55:07 +0600

    7.7

---
+++ add.js
@@ -0,0 +1,29 @@
+const orangePrice = 20;
+const mangoPrice = 20;
+const totalPrice = mangoPrice + orangePrice
+console.log(orangePrice)
+console.log(mangoPrice)
+console.log(totalPrice)
+
+
+const firstName = 'Tanvir'
+const lastName ='Ahsan'
+const fullName = firstName + lastName
+console.log(fullName)
+
+
+const numberString = '24.6'
+const number = parseInt(numberString)
+console.log(number)
+
+const numberStringg = '24.6'
+const numberr = parseFloat(numberStringg)
+console.log(numberr)
+
+
+const firstNumber = 0.1;
+const secondNumber = 0.3;
+const total = firstNumber + secondNumber
+const totalTwoDecimal = total.toFixed(2)
+const totalTwoDecimalNumber = parseFloat(totalTwoDecimal)
+console.log(totalTwoDecimalNumber)
---
+++ naming.js
@@ -0,0 +1,18 @@
+let location = 'BBan';
+let age = 18;
+let boyos = 18;
+let info ='1.Variable name should be something meaningful. It will indicate what it contains.Do not use shorthand.Its okey if the name is longer.)-----2.Cannot use a (reserved word) like let, const, false, true, etc.name should be in one word).-------3.Can not contain any space or gap.It can be type with snake case, camel case, pascal case)'
+// const her phone number =01965854522 //not allowed
+// const her_phone_number =01965854522 //snake case
+// const herPhoneNumber =01965854522 // camel case // this one we will use usally
+// const HerPhoneNumber =01965854522 //pascal case
+
+
+// 4.name cant stat with a number. But number could be use anywhere other than the first letter/.
+
+// 5. Other than letter can stat with either _ or $ . Cant use any other special character,
+
+// 6. Variable is case sensitive.
+let address = 'katabon'
+let Address = 'Sundarban'
+let AddRess = 'kochubon'
