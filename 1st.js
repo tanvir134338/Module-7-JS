da9efbd21b889c5143d13b6fd54bb2225166130f
tanvir134338 <tanvir134338@gmail.com>
2026-07-18 02:26:18 +0600

    first commit

---
+++ 1st.js
@@ -0,0 +1,16 @@
+console.log(3);
+
+// Variable
+var weight = 38;
+var temperature = 38;
+var people = 38;
+
+
+console.log(weight)
+
+// let, const
+let price = 100
+price = 50
+
+
+const PI = 3.14
